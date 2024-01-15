08f3f5061251e5ad0a9b87c1f88b8ebb10e4d71f
MicheleIanni86 <micheleianni86@gmail.com>
2024-01-15 19:21:09 +0100

    feat: Add Button next

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 console.log('ok');
 // HTML
 const slideContainerElement = document.getElementById('slideContainer');
+const arrowNextButton = document.querySelector('.arrow-next');
+const arrowPrevButton = document.querySelector('.arrow-prev');
+
 
 
 // creo array delle immagini
 const slides = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
+
 
 // immagine che mostro
 let photoView = 0;
@@ -30,4 +34,25 @@ for (let i = 0; i < slides.length; i++) {
 
 }
 
-slideContainerElement.innerHTML = immaginiHtml; 
+// slideContainerElement.innerHTML = immaginiHtml;
+
+// Bottone next
+arrowNextButton.addEventListener('click', function () {
+    // togli
+    const currentPhoto = document.querySelector('.slide.attiva');
+    currentPhoto.classList.remove('attiva');
+
+    photoView++;
+
+    // metti
+    const otherPhoto = document.getElementsByClassName('slide');
+    const newPhoto = otherPhoto[photoView];
+    newPhoto.classList.add('attiva');
+});
+
+
+// Bottone prev
+arrowPrevButton.addEventListener('click', function () {
+
+
+});
